80d064b7f1182cfaf089323ae6629141b7bca3c2
chison2707 <chiphansonzz17@gmail.com>
2025-06-06 14:17:50 +0700

    create account

--- backend/controllers/account.controller.js
+++ backend/controllers/account.controller.js
@@ -1,5 +1,6 @@
 import { pool } from "../config/database.js";
 
+// [GET]/api/v1/accoutns/:userId
 export const getAccounts = async (req, res) => {
     try {
         const userId = req.params.userId;
@@ -12,6 +13,70 @@ export const getAccounts = async (req, res) => {
         return res.json({
             status: 200,
             data: accounts.rows,
+        });
+    } catch (error) {
+        res.json({
+            status: 500,
+            message: error.message
+        });
+    }
+};
+
+export const createAccount = async (req, res) => {
+    try {
+        const userId = req.params.userId;
+
+        const { name, amount, account_number } = req.body;
+
+        const accountExistQuery = {
+            text: `SELECT * FROM tblaccount WHERE account_name = $1 AND user_id = $2`,
+            values: [name, userId],
+        };
+
+        const accountExistResult = await pool.query(accountExistQuery);
+
+        const accountExist = accountExistResult.rows[0];
+
+        if (accountExist) {
+            return res.json({
+                status: 400,
+                message: "Tài khoản ngân hàng đã được tạo!"
+            });
+        }
+
+        const createAccountResult = await pool.query({
+            text: `INSERT INTO tblaccount(user_id, account_name, account_number, account_balance) VALUES($1, $2, $3, $4) RETURNING *`,
+            values: [userId, name, account_number, amount],
+        });
+        const account = createAccountResult.rows[0];
+
+        const userAccounts = Array.isArray(name) ? name : [name];
+
+        const updateUserAccountQuery = {
+            text: `UPDATE tbluser SET accounts = array_cat(accounts, $1), updatedat = CURRENT_TIMESTAMP WHERE id = $2 RETURNING *`,
+            values: [userAccounts, userId],
+        };
+        await pool.query(updateUserAccountQuery);
+
+        const description = account.account_name + " (Tiền ban đầu)";
+
+        const initialDepositQuery = {
+            text: `INSERT INTO tbltransaction(user_id, description, type, status, amount, source) VALUES($1, $2, $3, $4, $5, $6) RETURNING *`,
+            values: [
+                userId,
+                description,
+                "income",
+                "Completed",
+                amount,
+                account.account_name,
+            ],
+        };
+        await pool.query(initialDepositQuery);
+
+        res.json({
+            status: 200,
+            message: "Tạo tài khoản thành công!",
+            data: account,
         });
     } catch (error) {
         res.json({
--- backend/routes/account.route.js
+++ backend/routes/account.route.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { getAccounts } from "../controllers/account.controller.js";
+import { createAccount, getAccounts } from "../controllers/account.controller.js";
 import { requireAuth } from "../middlewares/auth.middleware.js";
 
 const router = express.Router();
 router.get("/:userId", requireAuth, getAccounts);
+router.post("/create/:userId", requireAuth, createAccount);
 
 export default router;
