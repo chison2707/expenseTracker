f28b047c673102923f3bccfb0a52e9712b00ec43
chison2707 <chiphansonzz17@gmail.com>
2025-06-12 11:32:16 +0700

    api account

--- frontend/src/services/accountService.js
+++ frontend/src/services/accountService.js
@@ -1,6 +1,11 @@
-import { getAuth } from "../utils/requestAuth";
+import { getAuth, postAuth } from "../utils/requestAuth";
 
 export const getAccount = async (token) => {
     const result = await getAuth(`accounts`, token);
+    return result;
+}
+
+export const createAccount = async (options, token) => {
+    const result = await postAuth(`accounts/create`, options, token);
     return result;
 }
--- frontend/src/utils/requestAuth.js
+++ frontend/src/utils/requestAuth.js
@@ -25,3 +25,16 @@ export const patchAuth = async (path, data, token) => {
     const result = await response.json();
     return result;
 }
+
+export const postAuth = async (path, data, token) => {
+    const response = await fetch(API_DOMAIN + path, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${token}`,
+        },
+        body: JSON.stringify(data)
+    });
+    const result = await response.json();
+    return result;
+};
